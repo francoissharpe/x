a7f6c00bd35165d514f4600f26ed2243a7a08721
Francois Sharpe <francoissharpe@users.noreply.github.com>
2025-01-04 19:44:31 +0100

    feat: Add Procfile module support

---
+++ procfile/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"dagger/procfile/internal/dagger"
+	"fmt"
+	"html/template"
+	"strings"
+)
+
+// createDockerEntryPointSh returns a shell script that given an argument will start the process with the given name.
+func createDockerEntryPointSh(procs []Process) string {
+	// TODO: improve this script to handle SIGTERM and other signals and process reaping.
+	// Create a shell script that will start the process with the given name.
+	tmpl := `#!/bin/sh
+set -e
+case "$1" in
+{{- range . }}
+  {{ .Name }})
+	exec {{ .Command }}
+	;;
+{{- end }}
+  *)
+	echo "Invalid process name: $1"
+	exit 1
+	;;
+esac
+`
+	t, err := template.New("entrypoint").Parse(tmpl)
+	if err != nil {
+		panic(err)
+	}
+	var script strings.Builder
+	err = t.Execute(&script, procs)
+	if err != nil {
+		panic(err)
+	}
+	return script.String()
+}
+
+type Process struct {
+	Name    string
+	Command string
+}
+
+type Procfile struct{}
+
+func (m *Procfile) Parse(ctx context.Context, procfile *dagger.File) ([]Process, error) {
+	var procs []Process
+	procfileContents, err := procfile.Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+	// Parse the procfile and populate the procs slice.
+	for _, line := range strings.Split(procfileContents, "\n") {
+		// Split the line into process name and command.
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid procfile line: %s", line)
+		}
+		processName, command := strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
+		procs = append(procs, Process{Name: processName, Command: command})
+	}
+	return procs, nil
+}
+
+// Given a Procfile and container, return the container with the entrypoint configured to start with the given process.
+func (m *Procfile) WithProcfile(
+	ctx context.Context,
+	// The container to configure.
+	container *dagger.Container,
+	// The Procfile to use.
+	procfile *dagger.File,
+	// Path to the entrypoint script.
+	// +optional
+	// +default="/docker-entrypoint.sh"
+	entryPointPath string,
+) *dagger.Container {
+	procs, err := m.Parse(ctx, procfile)
+	if err != nil {
+		fmt.Errorf("failed to parse Procfile: %v", err)
+	}
+	return container.WithNewFile(entryPointPath, createDockerEntryPointSh(procs)).WithEntrypoint([]string{entryPointPath}).WithDefaultArgs([]string{procs[0].Name})
+}
